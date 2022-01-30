f08063a73dc308d4acfdbcd8c73fb6b20a45f1e9
Dionathan Chrys <98222996+dionathanchrys@users.noreply.github.com>
2022-01-30 20:19:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ name=Visual Studio Code
 baseurl=https://packages.microsoft.com/yumrepos/vscode
 enabled=1
 gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
 
 #Instalando programas
 yum install -y snapd
