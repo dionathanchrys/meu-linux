1290591c88277eea451373f5fcd6ae1a3df0be64
Dionathan Chrys <98222996+dionathanchrys@users.noreply.github.com>
2022-03-29 10:16:53 -0300

    ajustando echo com ###

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,32 +8,33 @@ read NOME
 hostname -b $NOME
 
 #Atualizando sistema
-echo " " && echo "Atualizando sistema (apt UPDATE)" && echo " "
+echo " " && echo "#######################################" && echo "Atualizando sistema (apt UPDATE)" && echo "#######################################" && echo " "
 apt update -y
-echo " " && echo "Atualizando sistema (apt UPGRADE)" && echo " "
+echo " " && echo "#######################################" && echo "Atualizando sistema (apt UPGRADE)" && echo "#######################################" && echo " "
 apt upgrade -y
 
 #Google Chrome
-echo " " && echo "Baixando Google Chrome" && echo " "
+echo " " && echo "#######################################" && echo "Baixando Google Chrome" && echo "#######################################" && echo " "
 wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 echo " " && echo "Instalando Google Chrome" && echo " "
 apt install -y /tmp/google-chrome-stable_current_amd64.deb
 
 #Anydesk
-echo " " && echo "Adicionando chave e repos do Anydesk" && echo " "
+echo " " && echo "#######################################" && echo "Adicionando chave e repos do Anydesk" && echo "#######################################" && echo " "
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
 
 #Instalando programas
-echo " " && echo "Instalando Anydesk" && echo " "
+echo " " && echo "#######################################" && echo "Instalando Anydesk" && echo "#######################################" && echo " "
 apt install -y anydesk
 
-echo " " && echo "Instalando OCS Agent" && echo " "
+echo " " && echo "#######################################" && echo "Instalando OCS Agent" && echo "#######################################" && echo " "
 echo "NA PROXIMA TELA, NA INSTALAÇÃO DO OCS ESCOLHA A OPÇÃO -> LOCAL <-"
+echo "Pressione enter para continuar" && read
 apt install -y ocsinventory-agent
 
 #Configs
-echo " " && echo "Configurando OCS Agent" && echo " "
+echo " " && echo "#######################################" && echo "Configurando OCS Agent" && echo "#######################################" && echo " "
 echo "server = http://10.10.10.10/ocsinventory
 # local = /var/lib/ocsinventory-agent
 tag = $NOME
@@ -41,9 +42,9 @@ tag = $NOME
 logger = Stderr
 logfile = /var/log/ocsinventory-agent/ocsinventory-agent.log" > /etc/ocsinventory/ocsinventory-agent.cfg
 
-echo " " && echo "Rodando OCS Agent" && echo " "
+echo " " && echo "#######################################" && echo "Rodando OCS Agent" && echo "#######################################" && echo " "
 ocsinventory-agent
 
-echo "Qual é o usuário para criar?"
+echo " " && echo "Qual é o usuário para criar?"
 read usuario
 adduser --force-badname $usuario
